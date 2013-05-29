8ef70d73be1aaf3f0912974339c1d73c55413b01
marc-inn <0.660162@gmail.com>
2013-05-29 18:24:51 +0200

    Piramidy gotowe, tylko wysłać sekwencje do inita

--- src/init/init_ui.cpp
+++ src/init/init_ui.cpp
@@ -8,18 +8,22 @@ Init_UI::Init_UI(common::Config *config_p, Client_UI *client_ui, QWidget *parent
     QWidget(parent),
     ui(new Ui::Init_UI()),
     cui(client_ui),
-    config(config_p)
+    config(config_p),
+    prevIndex(0)
 {
     ui->setupUi(this);
 
-    QStringList predatorsPyramide = {"NONE", "HUNTING", "EATING", "DRINKING", "PROCREATING"};
-    QStringList herbivorsPyramide = {"NONE", "RUNNING", "EATING", "DRINKING", "PROCREATING"};
+    predatorsPyramide = {"NONE", "HUNTING", "EATING", "DRINKING", "PROCREATING"};
+    herbivorsPyramide = {"NONE", "RUNNING", "EATING", "DRINKING", "PROCREATING"};
 
-    predatorsBool = { false, false, false, false, false };
-    herbivoresBool = { false, false, false, false, false };
+    predatorsTable = {0, 0, 0, 0};
+    herbivorsTable = {0, 0, 0, 0};
 
-    ui->comboBox->addItems(herbivorsPyramide);
+    ui->comboBox_1->addItems(herbivorsPyramide);
     ui->comboBox_2->addItems(herbivorsPyramide);
+    ui->comboBox_7->removeItem(index);
+    ui->comboBox_5->removeItem(index);
+    ui->comboBox_6->removeItem(index);
     ui->comboBox_3->addItems(herbivorsPyramide);
     ui->comboBox_4->addItems(herbivorsPyramide);
 
@@ -28,7 +32,15 @@ Init_UI::Init_UI(common::Config *config_p, Client_UI *client_ui, QWidget *parent
     ui->comboBox_7->addItems(predatorsPyramide);
     ui->comboBox_8->addItems(predatorsPyramide);
 
-    connect( ui->comboBox, SIGNAL( activated(int) ), this, SLOT(comboBox_Activated(int)) );
+    connect( ui->comboBox_1, SIGNAL( activated(int) ), this, SLOT(comboBox1_Activated(int)) );
+    connect( ui->comboBox_2, SIGNAL( activated(int) ), this, SLOT(comboBox2_Activated(int)) );
+    connect( ui->comboBox_3, SIGNAL( activated(int) ), this, SLOT(comboBox3_Activated(int)) );
+    connect( ui->comboBox_4, SIGNAL( activated(int) ), this, SLOT(comboBox4_Activated(int)) );
+
+    connect( ui->comboBox_5, SIGNAL( activated(int) ), this, SLOT(comboBox5_Activated(int)) );
+    connect( ui->comboBox_6, SIGNAL( activated(int) ), this, SLOT(comboBox6_Activated(int)) );
+    connect( ui->comboBox_7, SIGNAL( activated(int) ), this, SLOT(comboBox7_Activated(int)) );
+    connect( ui->comboBox_8, SIGNAL( activated(int) ), this, SLOT(comboBox8_Activated(int)) );
 }
 
 Init_UI::~Init_UI()
@@ -43,19 +55,292 @@ void Init_UI::on_pushButton_clicked()
     this->cui->show();
 }
 
-
-
-void Init_UI::comboBox_Activated(int index)
+void Init_UI::comboBox1_Activated(int index)
 {
-    ui->comboBox_2->removeItem(index);
-    ui->comboBox_3->removeItem(index);
-    ui->comboBox_4->removeItem(index);
+    if (index != 0)
+    {
+        // if h*T*[1] is different than zero enable button in other boxes
+        if (herbivorsTable[0] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in2 = ui->comboBox_2->model()->index(herbivorsTable[0], 0);
+            QModelIndex in3 = ui->comboBox_3->model()->index(herbivorsTable[0], 0);
+            QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[0], 0);
+            ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+            ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+            ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in2 = ui->comboBox_2->model()->index(index, 0);
+        QModelIndex in3 = ui->comboBox_3->model()->index(index, 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(index, 0);
+        ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+        ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+        ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in2 = ui->comboBox_2->model()->index(herbivorsTable[0], 0);
+        QModelIndex in3 = ui->comboBox_3->model()->index(herbivorsTable[0], 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[0], 0);
+        ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+        ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+        ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    herbivorsTable[0] = index;
 }
 
 
 void Init_UI::comboBox2_Activated(int index)
 {
-    ui->comboBox->removeItem(index);
-    ui->comboBox_3->removeItem(index);
-    ui->comboBox_4->removeItem(index);
+    if (index != 0)
+    {
+        // if h*T*[1] is different than zero enable button in other boxes
+        if (herbivorsTable[1] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in1 = ui->comboBox_1->model()->index(herbivorsTable[1], 0);
+            QModelIndex in3 = ui->comboBox_3->model()->index(herbivorsTable[1], 0);
+            QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[1], 0);
+            ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+            ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+            ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in1 = ui->comboBox_1->model()->index(index, 0);
+        QModelIndex in3 = ui->comboBox_3->model()->index(index, 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(index, 0);
+        ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+        ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+        ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in1 = ui->comboBox_1->model()->index(herbivorsTable[1], 0);
+        QModelIndex in3 = ui->comboBox_3->model()->index(herbivorsTable[1], 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[1], 0);
+        ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+        ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+        ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    herbivorsTable[1] = index;
+}
+
+void Init_UI::comboBox3_Activated(int index)
+{
+    if (index != 0)
+    {
+        // if h*T*[1] is different than zero enable button in other boxes
+        if (herbivorsTable[2] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in1 = ui->comboBox_1->model()->index(herbivorsTable[2], 0);
+            QModelIndex in2 = ui->comboBox_2->model()->index(herbivorsTable[2], 0);
+            QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[2], 0);
+            ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+            ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+            ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in1 = ui->comboBox_1->model()->index(index, 0);
+        QModelIndex in2 = ui->comboBox_2->model()->index(index, 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(index, 0);
+        ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+        ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+        ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in1 = ui->comboBox_1->model()->index(herbivorsTable[1], 0);
+        QModelIndex in2 = ui->comboBox_2->model()->index(herbivorsTable[1], 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[1], 0);
+        ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+        ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+        ui->comboBox_4->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    herbivorsTable[2] = index;
+}
+
+void Init_UI::comboBox4_Activated(int index)
+{
+    if (index != 0)
+    {
+        // if h*T*[1] is different than zero enable button in other boxes
+        if (herbivorsTable[3] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in1 = ui->comboBox_1->model()->index(herbivorsTable[3], 0);
+            QModelIndex in2 = ui->comboBox_2->model()->index(herbivorsTable[3], 0);
+            QModelIndex in3 = ui->comboBox_3->model()->index(herbivorsTable[3], 0);
+            ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+            ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+            ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in1 = ui->comboBox_1->model()->index(index, 0);
+        QModelIndex in2 = ui->comboBox_2->model()->index(index, 0);
+        QModelIndex in3 = ui->comboBox_3->model()->index(index, 0);
+        ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+        ui->comboBox_2->model()->setData(in2, v, Qt::UserRole -1);
+        ui->comboBox_3->model()->setData(in3, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in1 = ui->comboBox_1->model()->index(herbivorsTable[3], 0);
+        QModelIndex in3 = ui->comboBox_3->model()->index(herbivorsTable[3], 0);
+        QModelIndex in4 = ui->comboBox_4->model()->index(herbivorsTable[3], 0);
+        ui->comboBox_1->model()->setData(in1, v, Qt::UserRole -1);
+        ui->comboBox_2->model()->setData(in3, v, Qt::UserRole -1);
+        ui->comboBox_3->model()->setData(in4, v, Qt::UserRole -1);
+    }
+    herbivorsTable[3] = index;
+}
+
+void Init_UI::comboBox5_Activated(int index)
+{
+    if (index != 0)
+    {
+        // if h*T[1] is different than zero enable button in other boxes
+        if (predatorsTable[0] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in6 = ui->comboBox_6->model()->index(predatorsTable[4], 0);
+            QModelIndex in7 = ui->comboBox_7->model()->index(predatorsTable[4], 0);
+            QModelIndex in8 = ui->comboBox_8->model()->index(predatorsTable[4], 0);
+            ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+            ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+            ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in6 = ui->comboBox_6->model()->index(index, 0);
+        QModelIndex in7 = ui->comboBox_7->model()->index(index, 0);
+        QModelIndex in8 = ui->comboBox_8->model()->index(index, 0);
+        ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+        ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+        ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in6 = ui->comboBox_6->model()->index(predatorsTable[0], 0);
+        QModelIndex in7 = ui->comboBox_7->model()->index(predatorsTable[0], 0);
+        QModelIndex in8 = ui->comboBox_8->model()->index(predatorsTable[0], 0);
+        ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+        ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+        ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+    }
+    predatorsTable[0] = index;
+}
+
+void Init_UI::comboBox6_Activated(int index)
+{
+
+    if (index != 0)
+    {
+        // if h*T[1] is different than zero enable button in other boxes
+        if (predatorsTable[1] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in5 = ui->comboBox_6->model()->index(predatorsTable[1], 0);
+            QModelIndex in7 = ui->comboBox_7->model()->index(predatorsTable[1], 0);
+            QModelIndex in8 = ui->comboBox_8->model()->index(predatorsTable[1], 0);
+            ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+            ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+            ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in5 = ui->comboBox_5->model()->index(index, 0);
+        QModelIndex in7 = ui->comboBox_7->model()->index(index, 0);
+        QModelIndex in8 = ui->comboBox_8->model()->index(index, 0);
+        ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+        ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+        ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in5 = ui->comboBox_5->model()->index(predatorsTable[1], 0);
+        QModelIndex in7 = ui->comboBox_7->model()->index(predatorsTable[1], 0);
+        QModelIndex in8 = ui->comboBox_8->model()->index(predatorsTable[1], 0);
+        ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+        ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+        ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+    }
+    predatorsTable[1] = index;
+}
+
+void Init_UI::comboBox7_Activated(int index)
+{
+    if (index != 0)
+    {
+        // if h*T[1] is different than zero enable button in other boxes
+        if (predatorsTable[2] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in5 = ui->comboBox_5->model()->index(predatorsTable[2], 0);
+            QModelIndex in6 = ui->comboBox_6->model()->index(predatorsTable[2], 0);
+            QModelIndex in8 = ui->comboBox_8->model()->index(predatorsTable[2], 0);
+            ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+            ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+            ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in5 = ui->comboBox_5->model()->index(index, 0);
+        QModelIndex in6 = ui->comboBox_6->model()->index(index, 0);
+        QModelIndex in8 = ui->comboBox_8->model()->index(index, 0);
+        ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+        ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+        ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in5 = ui->comboBox_5->model()->index(predatorsTable[2], 0);
+        QModelIndex in6 = ui->comboBox_6->model()->index(predatorsTable[2], 0);
+        QModelIndex in8 = ui->comboBox_8->model()->index(predatorsTable[2], 0);
+        ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+        ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+        ui->comboBox_8->model()->setData(in8, v, Qt::UserRole -1);
+    }
+    predatorsTable[2] = index;
+}
+
+void Init_UI::comboBox8_Activated(int index)
+{
+    if (index != 0)
+    {
+        // if h*T[1] is different than zero enable button in other boxes
+        if (predatorsTable[3] != 0)
+        {
+            QVariant v(1|32);
+            QModelIndex in6 = ui->comboBox_6->model()->index(predatorsTable[3], 0);
+            QModelIndex in7 = ui->comboBox_7->model()->index(predatorsTable[3], 0);
+            QModelIndex in5 = ui->comboBox_5->model()->index(predatorsTable[3], 0);
+            ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+            ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+            ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+        }
+        QVariant v(0);
+        QModelIndex in6 = ui->comboBox_6->model()->index(index, 0);
+        QModelIndex in7 = ui->comboBox_7->model()->index(index, 0);
+        QModelIndex in5 = ui->comboBox_5->model()->index(index, 0);
+        ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+        ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+        ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+    }
+    else
+    {
+        QVariant v(1|32);
+        QModelIndex in6 = ui->comboBox_6->model()->index(predatorsTable[3], 0);
+        QModelIndex in7 = ui->comboBox_7->model()->index(predatorsTable[3], 0);
+        QModelIndex in5 = ui->comboBox_5->model()->index(predatorsTable[3], 0);
+        ui->comboBox_6->model()->setData(in6, v, Qt::UserRole -1);
+        ui->comboBox_7->model()->setData(in7, v, Qt::UserRole -1);
+        ui->comboBox_5->model()->setData(in5, v, Qt::UserRole -1);
+    }
+    predatorsTable[3] = index;
 }
--- src/init/init_ui.hpp
+++ src/init/init_ui.hpp
@@ -24,16 +24,25 @@ public:
     
 private slots:
     void on_pushButton_clicked();
-    void comboBox_Activated(int);
+    void comboBox1_Activated(int);
     void comboBox2_Activated(int);
+    void comboBox3_Activated(int);
+    void comboBox4_Activated(int);
+    void comboBox5_Activated(int);
+    void comboBox6_Activated(int);
+    void comboBox7_Activated(int);
+    void comboBox8_Activated(int);
 
 private:
-    Ui::Init_UI *ui;
-    Client_UI *cui;
-    common::Config *config;
+    Ui::Init_UI     *ui;
+    Client_UI       *cui;
+    common::Config  *config;
+    int             prevIndex;
 
-    QVector<bool> predatorsBool;
-    QVector<bool> herbivoresBool;
+    QStringList     predatorsPyramide;
+    QStringList     herbivorsPyramide;
+    QVector<int>    predatorsTable;
+    QVector<int>    herbivorsTable;
 };
 
 #endif // INIT_UI_H
