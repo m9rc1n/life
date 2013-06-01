fc7fb5042eca40f8ce14f47cc73aab1b90a0612b
marc-inn <0.660162@gmail.com>
2013-06-01 19:55:12 +0200

    Zoom działa narazie w jednym kierunku i prowizorycznie ale działa, skrolowanie nie

--- src/common/Map.cpp
+++ src/common/Map.cpp
@@ -6,9 +6,9 @@ void common::Map::accept(Visitor &visitor)
     // instead of: for(std::vector<common::MapObject*>::iterator iter = objects.begin(); iter != objects.begin(); ++iter)
     // visitor.visit(**iter);
 
-    Predator *predzio = new Predator(1, 1);
+    Predator *predzio = new Predator(52.9, 24.4);
 
-    Tree * dzewo = new Tree(3, 3);
+    Tree * dzewo = new Tree(52.1, 23.3);
 
     visitor.visit(*predzio);
     visitor.visit(*dzewo);
