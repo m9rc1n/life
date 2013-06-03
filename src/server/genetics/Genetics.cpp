82c39126bb5d22635ea9269727c7792077f2a874
mkrawczak <michalarturkrawczak@gmail.com>
2013-06-03 16:31:34 +0200

    Genetyka c.d.

--- src/server/genetics/Genetics.cpp
+++ src/server/genetics/Genetics.cpp
@@ -1,11 +1,19 @@
 #include "Genetics.hpp"
-
+#include "Genotype.hpp"
 namespace server
 {
+    const int Genetics::MUTATION_COUNT = 10;
 
     void Genetics::makeChild(common::Creature *mother, common::Creature *father)
     {
+        server::Genotype mother_genotype (mother);
+        server::Genotype father_genotype (father);
 
+        server::Genotype child_Genotype (mother_genotype, father_genotype);
+        for(int i=0; i<Genetics::MUTATION_COUNT; ++i)
+        {
+            child_Genotype.mutate();
+        }
     }
 
 } // namespace server
--- src/server/genetics/Genome.cpp
+++
@@ -1,9 +0,0 @@
-#include "Genome.hpp"
-
-namespace server {
-
-Genome::Genome(common::Creature *creature)
-{
-}
-
-} // namespace server
